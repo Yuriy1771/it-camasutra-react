9236eceada4b4368552587d4f9fba24d43906d68
Tank Nigella <dementev.yuriy@list.ru>
2025-01-20 14:08:16 +0300

    doned second container component for axios

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,6 +1,34 @@
 import {connect} from "react-redux";
-import Friends from "./Friends";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/friendsReducer";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
+
+class FriendsAPIContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.state.currentPage}&count=${this.props.state.countUsersOfPage}`)
+            .then(response => {
+                this.props.setUsers(
+                    response.data.items
+                )
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onCurrentPageClick = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.state.countUsersOfPage}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <Users state={this.props.state} follow={this.props.follow} unfollow={this.props.unfollow}
+                      setCurrentPage={this.props.setCurrentPage} setTotalUsersCount={this.props.setTotalUsersCount}
+                      onCurrentPageClick={this.onCurrentPageClick}/>
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -18,6 +46,6 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends)
+const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsAPIContainer)
 
 export default FriendsContainer
---
+++ src/components/Friends/Users.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styles from "./Friends.module.css";
+import user_avatar from "../../assets/images/user_avatar.png";
+
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.state.totalUsersCount / props.state.countUsersOfPage)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+
+    return (
+        <div>
+            {props.state.users.map((user) => <div className={styles.userBlock} key={user.id}>
+                <div className={styles.avatarAndIsFollowBlock}>
+                    <div>
+                        <div><img className={styles.userAvatar} src={user.photos.small != null
+                            ? user.photos.small
+                            : user_avatar} alt="img"/></div>
+                    </div>
+                    <div className={styles.isFollowBtn}>
+                        {user.followed
+                            ? <button onClick={() => {
+                                props.follow(user.id)
+                            }} className={styles.unfollow}>unfollow</button>
+                            : <button onClick={() => {
+                                props.unfollow(user.id)
+                            }} className={styles.follow}>follow</button>}
+                    </div>
+                </div>
+                <div className={styles.userNameBlock}>
+                    <div>{user.name}</div>
+                    <div>{user.status}</div>
+                </div>
+                <div className={styles.userLocation}>
+                    <div>Russia</div>
+                    <div>Kazan</div>
+                </div>
+            </div>)}
+            <div className={styles.pagination}>
+                {pages.map((page) => <div onClick={() => props.onCurrentPageClick(page)}
+                                          className={props.state.currentPage === page ? styles.selectedPage : styles.numberPage}>{page}</div>)}
+            </div>
+        </div>
+    )
+}
+
+export default Users
