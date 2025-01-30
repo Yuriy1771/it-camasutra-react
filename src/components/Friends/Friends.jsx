3fcbe1e289b40547613a374db62586f5b7f5d23b
Tank Nigella <dementev.yuriy@list.ru>
2025-01-30 17:23:53 +0300

    done pagination component and stylized it

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -4,18 +4,13 @@ import Friend from "./Friend";
 
 const Friends = ({totalUsersCount, countUsersOfPage, followThunk, unfollowThunk,
                      users, isDisabledFollow, currentPage, onCurrentPageClick}) => {
-
-    let pagesCount = Math.ceil(totalUsersCount / countUsersOfPage)
-    let pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
-    }
     return (
         <div>
             {users.map((user) => <Friend key={user.id} user={user} isDisabledFollow={isDisabledFollow}
                                          followThunk={followThunk} unfollowThunk={unfollowThunk}/>)
             }
-            <Pagination pages={pages} currentPage={currentPage} onCurrentPageClick={onCurrentPageClick}/>
+            <Pagination currentPage={currentPage} onCurrentPageClick={onCurrentPageClick}
+                        totalUsersCount={totalUsersCount} countUsersOfPage={countUsersOfPage}/>
         </div>
     )
 }
--- src/components/other/Pagination/Pagination.jsx
+++ src/components/other/Pagination/Pagination.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from "./Pagination.module.css";
 
-const Pagination = ({pages, currentPage, onCurrentPageClick}) => {
+const Pagination = ({currentPage, onCurrentPageClick, totalUsersCount, countUsersOfPage, portionSize = 10}) => {
+
+    let pagesCount = Math.ceil(totalUsersCount / countUsersOfPage)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+
+    useEffect(() => setPortionNumber(Math.ceil(currentPage / portionSize)),[currentPage, portionSize])
+
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionPageNumber = (portionNumber -1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
     return (
         <>
             <div className={styles.pagination}>
-                {pages.map((page) => <div onClick={() => onCurrentPageClick(page)}
+                {portionNumber > 0 &&
+                    <button onClick={() => setPortionNumber(portionNumber - 1)} disabled={portionNumber === 1} className={styles.prevBtn}>{'<<'}</button>}
+                {
+                    pages.filter((page) => page >= leftPortionPageNumber && page<= rightPortionPageNumber)
+                        .map((page) => <div onClick={() => onCurrentPageClick(page)}
                                           className={currentPage === page ? styles.selectedPage : styles.numberPage}>{page}</div>)}
+                {portionCount > portionNumber &&
+                    <button onClick={() => setPortionNumber(portionNumber +1)} className={styles.nextBtn}>{'>>'}</button>}
             </div>
         </>
     )
