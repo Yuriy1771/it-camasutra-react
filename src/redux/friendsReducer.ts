e6e805ac72af0c164dd7a78f483850d328e13007
Tank Nigella <dementev.yuriy@list.ru>
2025-02-07 17:28:55 +0300

    type actions in friendsReducer

--- src/redux/friendsReducer.ts
+++ src/redux/friendsReducer.ts
@@ -2,15 +2,15 @@ import {resultCodesEnum, usersAPI} from "../api/api.ts";
 import {updateObjectInArray} from "../utils/helpers/objectHelpers.js";
 import {photosType, usersType} from "../types/types";
 import {Dispatch} from "redux";
-import {appStateType} from "./redux-store";
+import {appStateType, inferActionsTypes} from "./redux-store";
 
-const FOLLOW = 'friends/FOLLOW'
-const UNFOLLOW = 'friends/UNFOLLOW'
-const SET_USERS = 'friends/SET_USERS'
-const SET_CURRENT_PAGE = 'friends/SET_CURRENT_PAGE'
-const SET_USERS_TOTAL_COUNT = 'friends/SET_USERS_TOTAL_COUNT'
-const SET_PRELOADER = 'friends/SET_PRELOADER'
-const IS_DISABLED_FOLLOW = 'friends/IS_DISABLED_FOLLOW'
+// const FOLLOW = 'friends/FOLLOW'
+// const UNFOLLOW = 'friends/UNFOLLOW'
+// const SET_USERS = 'friends/SET_USERS'
+// const SET_CURRENT_PAGE = 'friends/SET_CURRENT_PAGE'
+// const SET_USERS_TOTAL_COUNT = 'friends/SET_USERS_TOTAL_COUNT'
+// const SET_PRELOADER = 'friends/SET_PRELOADER'
+// const IS_DISABLED_FOLLOW = 'friends/IS_DISABLED_FOLLOW'
 
 type initialStateType = typeof initialState
 let initialState: initialStateType = {
@@ -25,25 +25,25 @@ let initialState: initialStateType = {
 const friendsReducer = (state = initialState, action: actionsType): initialStateType => {
     let stateCopy
     switch (action.type) {
-        case FOLLOW:
+        case 'FOLLOW':
             return stateCopy = {
                 ...state,
                 users: updateObjectInArray(state.users, action.userId, {followed: true})
             }
-        case UNFOLLOW:
+        case 'UNFOLLOW':
             return stateCopy = {
                 ...state,
                 users: updateObjectInArray(state.users, action.userId, {followed: false})
             }
-        case SET_USERS:
+        case 'SET_USERS':
             return stateCopy = {...state, users: action.users}
-        case SET_USERS_TOTAL_COUNT:
+        case 'SET_USERS_TOTAL_COUNT':
             return stateCopy = {...state, totalUsersCount: action.usersCount}
-        case SET_CURRENT_PAGE:
+        case 'SET_CURRENT_PAGE':
             return stateCopy = {...state, currentPage: action.currentPage}
-        case SET_PRELOADER:
+        case 'SET_PRELOADER':
             return stateCopy = {...state, isLoader: action.isLoader}
-        case IS_DISABLED_FOLLOW:
+        case 'IS_DISABLED_FOLLOW':
             return stateCopy = {
                 ...state, isDisabledFollow: action.isDisabledFollow
                     ? [...state.isDisabledFollow, action.id]
@@ -52,58 +52,44 @@ const friendsReducer = (state = initialState, action: actionsType): initialState
     }
     return state
 }
-type actionsType = (followACType | unfollowACType | setUsersACType | setCurrentPageACType | setTotalUsersCount |
-    setPreloaderACType | setIsDisabledFollowACType)
+type actionsType = inferActionsTypes<typeof actions>
 
-type followACType = { type: typeof FOLLOW, userId: number }
-type unfollowACType = { type: typeof UNFOLLOW, userId: number }
-type setUsersACType = { type: typeof SET_USERS, users: usersType }
-type setCurrentPageACType = { type: typeof SET_CURRENT_PAGE, currentPage: number }
-type setTotalUsersCount = { type: typeof SET_USERS_TOTAL_COUNT, usersCount: number }
-type setPreloaderACType = { type: typeof SET_PRELOADER, isLoader: boolean }
-type setIsDisabledFollowACType = { type: typeof IS_DISABLED_FOLLOW, isDisabledFollow: boolean, id: number }
-
-export const follow = (userId: number): followACType => ({type: FOLLOW, userId})
-export const unfollow = (userId: number): unfollowACType => ({type: UNFOLLOW, userId})
-export const setUsers = (users: usersType): setUsersACType => ({type: SET_USERS, users})
-export const setCurrentPage = (currentPage: number): setCurrentPageACType => ({type: SET_CURRENT_PAGE, currentPage})
-export const setTotalUsersCount = (usersCount: number): setTotalUsersCount => ({
-    type: SET_USERS_TOTAL_COUNT,
-    usersCount
-})
-export const setPreloader = (isLoader: boolean): setPreloaderACType => ({type: SET_PRELOADER, isLoader})
-export const setIsDisabledFollow = (isDisabledFollow: boolean, id: number): setIsDisabledFollowACType => ({
-    type: IS_DISABLED_FOLLOW,
-    isDisabledFollow,
-    id
-})
+export const actions = {
+    follow: (userId: number) => ({type: 'FOLLOW', userId} as const),
+    unfollow: (userId: number) => ({type: 'UNFOLLOW', userId} as const),
+    setUsers: (users: usersType) => ({type: 'SET_USERS', users} as const),
+    setCurrentPage: (currentPage: number) => ({type: 'SET_CURRENT_PAGE', currentPage} as const),
+    setTotalUsersCount: (usersCount: number) => ({type: 'SET_USERS_TOTAL_COUNT', usersCount} as const),
+    setPreloader: (isLoader: boolean) => ({type: 'SET_PRELOADER', isLoader} as const),
+    setIsDisabledFollow: (isDisabledFollow: boolean, id: number) => ({type: 'IS_DISABLED_FOLLOW', isDisabledFollow, id} as const),
+}
 
 export const getUsersThunk = (currentPage: number, countUsersOfPage: number) =>
     async (dispatch: Dispatch<actionsType>, getState: () => appStateType) => {
-        dispatch(setPreloader(true))
+        dispatch(actions.setPreloader(true))
         const responseUsers = await usersAPI.getUsersAPI(currentPage, countUsersOfPage)
-        dispatch(setPreloader(false))
-        dispatch(setUsers(responseUsers.items))
-        dispatch(setCurrentPage(currentPage))
-        dispatch(setTotalUsersCount(responseUsers.totalCount))
+        dispatch(actions.setPreloader(false))
+        dispatch(actions.setUsers(responseUsers.items))
+        dispatch(actions.setCurrentPage(currentPage))
+        dispatch(actions.setTotalUsersCount(responseUsers.totalCount))
     }
 
 const followUnfollow = async (dispatch: Dispatch<actionsType>, id: number, apiMethod: any,
-                              actionCreator: (id: number) => followACType | unfollowACType) => {
-    dispatch(setIsDisabledFollow(true, id))
+                              actionCreator: (id: number) => actionsType) => {
+    dispatch(actions.setIsDisabledFollow(true, id))
     const responseUnfollow = await apiMethod(id)
     if (responseUnfollow.resultCode === resultCodesEnum.success) {
         dispatch(actionCreator(id))
     }
-    dispatch(setIsDisabledFollow(false, id))
+    dispatch(actions.setIsDisabledFollow(false, id))
 }
 
 export const followThunk = (id: number) => async (dispatch: Dispatch<actionsType>, getState: () => appStateType) => {
-    await followUnfollow(dispatch, id, usersAPI.followAPI.bind(usersAPI), follow)
+    await followUnfollow(dispatch, id, usersAPI.followAPI.bind(usersAPI), actions.follow)
 }
 
 export const unfollowThunk = (id: number) => async (dispatch: Dispatch<actionsType>, getState: () => appStateType) => {
-    await followUnfollow(dispatch, id, usersAPI.unfollowAPI.bind(usersAPI), unfollow)
+    await followUnfollow(dispatch, id, usersAPI.unfollowAPI.bind(usersAPI), actions.unfollow)
 }
 
 export default friendsReducer
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -17,6 +17,9 @@ let rootReducer = combineReducers({
 type rootReducerType = typeof rootReducer
 export type appStateType = ReturnType<rootReducerType>
 
+type propertiesTypes<T> = T extends {[key: string]:infer U} ? U : never
+export type inferActionsTypes<T extends {[key: string]:(...args: any[]) => any}> = ReturnType<propertiesTypes<T>>
+
 //@ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
