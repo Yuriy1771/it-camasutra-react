6af066d08bdf6e2f0c6363316dfc6596931de124
Tank Nigella <dementev.yuriy@list.ru>
2025-02-10 18:53:06 +0300

    refactor MyPosts and MyPostsContainer: added useSelector and useDispatch

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {FC} from 'react'
 import styles from './MyPosts.module.css'
 import Post from './Post/Post.tsx'
-import AddPostForm from "./addPostForm.jsx";
+import AddPostForm from "./addPostForm.tsx";
 import {reduxForm} from "redux-form";
-import {FC} from "react";
 import {postsType} from "../../../types/types";
+import {useDispatch, useSelector} from "react-redux";
+import {appStateType} from "../../../redux/redux-store";
+import {addPostAC} from "../../../redux/profileReducer.ts";
 
-type propsType = {addPost:(text:string) => void, posts: postsType}
+type propsType = {}
 const AddPostFormRedux = reduxForm({form: 'newTextPost'})(AddPostForm)
 
-const MyPosts:FC<propsType> = React.memo(({addPost, posts}) => {
+const MyPosts:FC<propsType> = React.memo((props) => {
+
+    const posts:postsType[] = useSelector((state:appStateType) => state.profilePage.posts)
+    const dispatch = useDispatch()
+
     let componentPosts = posts.map((post) => {
         return <Post message={post.postMessage} likes_count={post.likesCount} key={post.id}/>
     })
 
     const onSubmit = (formData):void => {
-        addPost(formData.newTextPost)
+        dispatch(addPostAC(formData.newTextPost))
     }
     return (
         <div className={styles.myPosts}>
--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,26 +1,11 @@
 import MyPosts from "./MyPosts.tsx";
-import {connect} from "react-redux";
-import {addPostAC} from "../../../redux/profileReducer.ts";
-import {postsType} from "../../../types/types";
-import {appStateType} from "../../../redux/redux-store";
 
-type mapStatePropsType = {posts: postsType}
-type mapDispatchPropsType = {addPost: (postText:string) => void}
-
-let mapStateToProps = (state):mapStatePropsType => {
-    return {
-        posts: state.profilePage.posts,
-    }
+const MyPostsContainer = (props) => {
+    return (
+        <>
+            <MyPosts/>
+        </>
+    )
 }
-
-let mapDispatchToProps = (dispatch):mapDispatchPropsType => {
-    return {
-        addPost: (postText):void => {
-            dispatch(addPostAC(postText))
-        },
-    }
-}
-
-const MyPostsContainer = connect<mapStatePropsType, mapDispatchPropsType, appStateType>(mapStateToProps, mapDispatchToProps)(MyPosts)
 
 export default MyPostsContainer
