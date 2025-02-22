ac9308d415cc82e3019420668eab580ad9dd6f12
Tank Nigella <tanknige@va-c5.kzn.21-school.ru>
2025-02-22 12:00:40 +0300

    refactor

--- src/components/Friends/Friend.tsx
+++ src/components/Friends/Friend.tsx
@@ -4,45 +4,50 @@ import {NavLink} from "react-router-dom";
 import user_avatar from "../../assets/images/userAvatar3.png";
 import {usersType} from "../../types/types";
 
-type friendType = {user: usersType, isDisabledFollow:number[], follow:(id:number) => void, unfollow:(id:number) => void}
-const Friend:FC<friendType> = ({user, isDisabledFollow, follow, unfollow}) => {
-    const onClickFollow = (id):void => {
+type friendType = {
+    user: usersType,
+    isDisabledFollow: number[],
+    follow: (id: number) => void,
+    unfollow: (id: number) => void
+}
+const Friend: FC<friendType> = ({user, isDisabledFollow, follow, unfollow}) => {
+    const onClickFollow = (id): void => {
         follow(id)
     }
 
-    const onClickUnfollow = (id):void => {
+    const onClickUnfollow = (id): void => {
         unfollow(id)
     }
     return (
         <div className={styles.userBlock}>
-                <div className={styles.avatarAndIsFollowBlock}>
+            <div className={styles.avatarAndIsFollowBlock}>
+                <div>
                     <div>
-                        <div>
-                            <NavLink to={`/profile/${user.id}`}>
-                                <img className={styles.userAvatar} src={user.photos.small != null
-                                    ? user.photos.small
-                                    : user_avatar} alt="img"/>
-                            </NavLink>
-                        </div>
-                    </div>
-                    <div className={styles.isFollowBtn}>
-                        {user.followed
-                            ?
-                            <button disabled={isDisabledFollow.some(id => id === user.id)}
-                                    onClick={() => onClickUnfollow(user.id)} className={styles.unfollow}>unfollow</button>
-                            :
-                            <button disabled={isDisabledFollow.some(id => id === user.id)}
-                                    onClick={() => onClickFollow(user.id)} className={styles.follow}>follow</button>}
+                        <NavLink to={`/profile/${user.id}`}>
+                            <img className={styles.userAvatar} src={user.photos.small != null
+                                ? user.photos.small
+                                : user_avatar} alt="img"/>
+                        </NavLink>
                     </div>
                 </div>
-                <div className={styles.userNameBlock}>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div>
+                <div className={styles.isFollowBtn}>
+                    {user.followed
+                        ?
+                        <button disabled={isDisabledFollow.some(id => id === user.id)}
+                                onClick={() => onClickUnfollow(user.id)} className={styles.unfollow}>unfollow</button>
+                        :
+                        <button disabled={isDisabledFollow.some(id => id === user.id)}
+                                onClick={() => onClickFollow(user.id)} className={styles.follow}>follow</button>}
                 </div>
-                <div className={styles.userLocation}>
-                    <div>Russia</div>
-                    <div>Kazan</div>
-                </div>
+            </div>
+            <div className={styles.userNameBlock}>
+                <div>{user.name}</div>
+                <div>{user.status}</div>
+            </div>
+            <div className={styles.userLocation}>
+                <div>Russia</div>
+                <div>Kazan</div>
+            </div>
         </div>
     )
 }
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,52 +1,11 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
-import styles from './Settings.module.css'
 
 const Settings = (props) => {
-    const {
-        register,
-        formState: {
-            errors,
-            isValid,
-        },
-        handleSubmit,
-        reset,
-    } = useForm({mode: 'onBlur'})
 
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data))
-        reset()
-    }
 
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>
-                    first name
-                    <input {...register('firstName', {
-                        required: 'Поле обязатеьное',
-                        minLength: {
-                            value: 3,
-                            message: 'минимум 3 символа'
-                        }
-                    })}/>
-                </label>
-                <label>
-                    last name
-                    <input {...register('lastName', {
-                        required: 'Field require',
-                        minLength: {
-                            value: 3,
-                            message: 'minimum 3 symbols'
-                        }
-                    })}/>
-                </label>
-                <input type="submit" disabled={isValid === false}/>
-                <div>
-                    {errors?.firstName && <span>{errors?.firstName?.message || 'error'}</span>
-                        || errors?.lastName && <span>{errors?.lastName?.message || 'error'}</span>}
-                </div>
-            </form>
+            settings
         </div>
     )
 }
